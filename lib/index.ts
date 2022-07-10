7e501e1c18afd726032c55d32bc0fbd64a61f206
Kyle Harding <kyle@balena.io>
2022-07-10 12:02:27 -0400

    Mount resin-data partition on init
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- lib/index.ts
+++ lib/index.ts
@@ -1,34 +1,65 @@
 import { schedule } from 'node-cron';
 import { doBackup, doPrune } from './renovate';
 import { logger } from './logger';
+import { spawn } from 'child_process';
+import { BACKUP_CRON, DATA_PART_LABEL, BIND_ROOT_PATH } from './config';
 
-const BACKUP_CRON = process.env.BACKUP_CRON;
-
-const args = ['--tag=scheduled'];
+const resticArgs = ['--tag=scheduled'];
 
 if (process.env.BALENA_APP_NAME) {
-	args.push(`--tag=${process.env.BALENA_APP_NAME}`);
+	resticArgs.push(`--tag=${process.env.BALENA_APP_NAME}`);
 }
 
 if (process.env.BALENA_DEVICE_NAME_AT_INIT) {
-	args.push(`--tag=${process.env.BALENA_DEVICE_NAME_AT_INIT}`);
+	resticArgs.push(`--tag=${process.env.BALENA_DEVICE_NAME_AT_INIT}`);
 }
 
 if (process.env.BALENA_DEVICE_UUID) {
-	args.push(`--host=${process.env.BALENA_DEVICE_UUID}`);
+	resticArgs.push(`--host=${process.env.BALENA_DEVICE_UUID}`);
 }
+
+const childProcess = async (cmd: string, args: string[]) => {
+	const p = spawn(cmd, args);
+
+	p.stdout.on('data', function (data) {
+		logger.info(data.toString());
+	});
+
+	p.stderr.on('data', function (data) {
+		logger.error(data.toString());
+	});
+
+	p.on('exit', function (code) {
+		if (code) {
+			logger.info('child process exited with code ' + code.toString());
+		} else {
+			logger.warn('child process exited');
+		}
+	});
+};
+
+const dryRun = async () => {
+	logger.info('Starting dry-run backup...');
+	return doBackup(['--dry-run', ...resticArgs]);
+};
 
 if (BACKUP_CRON) {
 	logger.info(`Scheduling backup with cron '${BACKUP_CRON}'`);
 	schedule(BACKUP_CRON, async () => {
 		logger.info('Starting scheduled backup...');
-		return doBackup(args).then(() => doPrune(args));
+		return doBackup(resticArgs).then(() => doPrune(resticArgs));
 	});
 }
 
-const dryRun = async () => {
-	logger.info('Starting dry-run backup...');
-	return doBackup(['--dry-run', ...args]);
-};
-
-dryRun();
+(async () => {
+	try {
+		await childProcess('mount', [
+			'-v',
+			`$(blkid -L ${DATA_PART_LABEL})`,
+			`${BIND_ROOT_PATH}`,
+		]);
+		await dryRun();
+	} catch (e) {
+		logger.error(e);
+	}
+})();
