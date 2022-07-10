a985d1da72bfbeb0855848bb98eee808e595b9a6
Kyle Harding <kyle@balena.io>
2022-07-10 15:18:40 -0400

    Exclude volumes used by restic
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- lib/renovate.ts
+++ lib/renovate.ts
@@ -1,8 +1,21 @@
-import { DATA_ROOT_DIR, BACKUP_OPTS, RESTORE_OPTS, PRUNE_OPTS } from './config';
-import { getStateStatus, stopServices, startServices } from './supervisor';
+import {
+	DATA_ROOT_DIR,
+	BACKUP_OPTS,
+	RESTORE_OPTS,
+	PRUNE_OPTS,
+	RESTIC_CACHE_DIR,
+	RESTIC_REPOSITORY,
+} from './config';
+import {
+	getStateStatus,
+	stopServices,
+	startServices,
+	getLocalTargetState,
+} from './supervisor';
 import { logger } from './logger';
 import { boolean } from 'boolean';
 import { childProcess } from './spawn';
+import { promises as fsPromises } from 'fs';
 
 const prependExtraArgs = (args: string[], extra: string[]): string[] => {
 	// put dry-run at the front of the extra args
@@ -24,9 +37,53 @@ const prependExtraArgs = (args: string[], extra: string[]): string[] => {
 	return extra.concat(args);
 };
 
+const getExcludedVolumes = async (
+	appId: string,
+	serviceName: string,
+): Promise<string[]> => {
+	const targetState = await getLocalTargetState();
+
+	const resticVolumes = targetState.state.local.apps[appId].services
+		.find((f: any) => f.serviceName === serviceName)
+		.config.volumes.filter((f: string) =>
+			[RESTIC_CACHE_DIR, RESTIC_REPOSITORY].includes(f.split(':')[1]),
+		)
+		.map((m: string) => m.split(':').shift());
+
+	const eligibleVolumes = Object.values(
+		targetState.state.local.apps[appId].volumes,
+	)
+		.map((m: any) => [m.appId, m.name].join('_'))
+		.filter((f) => !resticVolumes.includes(f));
+
+	const allVolumes = (
+		await fsPromises.readdir(DATA_ROOT_DIR, { withFileTypes: true })
+	)
+		.filter((dirent) => dirent.isDirectory())
+		.map((dirent) => dirent.name);
+
+	return allVolumes.filter((f) => !eligibleVolumes.includes(f));
+};
+
 // https://restic.readthedocs.io/en/latest/040_backup.html
 export const doBackup = async (args: string[] = []): Promise<string> => {
 	args = prependExtraArgs(args, BACKUP_OPTS);
+
+	if (
+		process.env.BALENA_APP_ID != null &&
+		process.env.BALENA_SERVICE_NAME != null
+	) {
+		const excludeVolumes = await getExcludedVolumes(
+			process.env.BALENA_APP_ID,
+			process.env.BALENA_SERVICE_NAME,
+		);
+		args = prependExtraArgs(
+			args,
+			excludeVolumes.map(
+				(m: any) => '--exclude=' + [DATA_ROOT_DIR, m].join('/'),
+			),
+		);
+	}
 
 	return childProcess('sh', [
 		'-c',
