0dc80c5622aaf99fc905fa91089ee96d3b741faf
Kyle Harding <kyle@balena.io>
2022-09-11 19:50:15 -0400

    Set container hostname and refactor config
    Change-type: minor
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,34 @@
-export const RESTIC_CACHE_DIR = process.env.RESTIC_CACHE_DIR || '/cache';
-export const RESTIC_REPOSITORY = process.env.RESTIC_REPOSITORY || '/snapshots';
+import { boolean } from 'boolean';
+
+export const RESTIC_CACHE_DIR = process.env.RESTIC_CACHE_DIR;
+export const RESTIC_REPOSITORY = process.env.RESTIC_REPOSITORY;
+
+export const BALENA_APP_ID = process.env.BALENA_APP_ID || '';
+export const BALENA_DEVICE_UUID = process.env.BALENA_DEVICE_UUID || '';
+export const BALENA_SERVICE_NAME = process.env.BALENA_SERVICE_NAME || '';
+
+export const SUPERVISOR_ADDRESS = process.env.BALENA_SUPERVISOR_ADDRESS;
+export const SUPERVISOR_API_KEY = process.env.BALENA_SUPERVISOR_API_KEY;
 
 export const DATA_PART_LABEL = process.env.DATA_PART_LABEL || 'resin-data';
 export const DATA_MOUNT_PATH = process.env.DATA_MOUNT_PATH || '/data';
 export const DATA_ROOT_DIR =
 	process.env.DATA_ROOT_DIR || DATA_MOUNT_PATH + '/docker/volumes';
+
+export const BACKUP_CRON = process.env.BACKUP_CRON;
+
+export const BACKUP_OPTS = process.env.BACKUP_OPTS?.split(/\s+/) || [
+	'--tag=scheduled',
+];
+
+export const PRUNE_OPTS = process.env.PRUNE_OPTS?.split(/\s+/) || [
+	'--keep-hourly=24',
+	'--keep-daily=7',
+	'--keep-weekly=5',
+	'--keep-monthly=12',
+	'--group-by=hosts,tags,path',
+];
+
+export const DRY_RUN = boolean(process.env.DRY_RUN);
+
+export const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
--- lib/index.ts
+++ lib/index.ts
@@ -1,36 +1,60 @@
 import { schedule } from 'node-cron';
 import { doBackup, doPrune } from './restic';
 import { logger } from './logger';
-import { DATA_PART_LABEL, DATA_MOUNT_PATH } from './config';
+import {
+	BACKUP_CRON,
+	BACKUP_OPTS,
+	BALENA_DEVICE_UUID,
+	DRY_RUN,
+	PRUNE_OPTS,
+	DATA_PART_LABEL,
+	DATA_MOUNT_PATH,
+} from './config';
 import { childProcess } from './spawn';
 import * as fs from 'fs';
 
-const BACKUP_CRON = process.env.BACKUP_CRON;
-
-const RESTIC_ARGS = ['--tag=scheduled'];
-
-if (process.env.BALENA_APP_NAME != null) {
-	RESTIC_ARGS.push(`--tag=${process.env.BALENA_APP_NAME}`);
+if (!BALENA_DEVICE_UUID) {
+	throw new Error('BALENA_DEVICE_UUID is required!');
 }
 
-if (process.env.BALENA_DEVICE_NAME_AT_INIT != null) {
-	RESTIC_ARGS.push(`--tag=${process.env.BALENA_DEVICE_NAME_AT_INIT}`);
+if (!process.env.RESTIC_PASSWORD) {
+	throw new Error('RESTIC_PASSWORD is required!');
 }
 
-if (process.env.BALENA_DEVICE_UUID != null) {
-	RESTIC_ARGS.push(`--host=${process.env.BALENA_DEVICE_UUID}`);
-}
+const extraArgs = (): string[] => {
+	const extra: string[] = [];
+
+	// put dry-run at the front of the extra args
+	if (DRY_RUN) {
+		extra.unshift('--dry-run');
+	}
+
+	// append one level of verbosity
+	if (logger.isInfoEnabled()) {
+		extra.unshift('-v');
+	}
+
+	// append one more level of verbosity
+	if (logger.isDebugEnabled()) {
+		extra.unshift('-v');
+	}
+
+	// give the passed-in args the highest priority by appending them last
+	return extra;
+};
 
 const dryRun = async () => {
 	logger.info('Starting dry-run backup...');
-	return doBackup(['--dry-run', ...RESTIC_ARGS]);
+	return doBackup(['--dry-run', '--tag=dry-run']);
 };
 
 if (BACKUP_CRON) {
 	logger.info(`Scheduling backup with cron '${BACKUP_CRON}'`);
 	schedule(BACKUP_CRON, async () => {
 		logger.info('Starting scheduled backup...');
-		return doBackup(RESTIC_ARGS).then(() => doPrune(RESTIC_ARGS));
+		return doBackup([...BACKUP_OPTS, ...extraArgs()]).then(() =>
+			doPrune([...PRUNE_OPTS, ...extraArgs()]),
+		);
 	});
 }
 
@@ -40,8 +64,14 @@ if (BACKUP_CRON) {
 			fs.mkdirSync(DATA_MOUNT_PATH, { recursive: true });
 		}
 
-		const dev = await await childProcess('blkid', ['-L', `${DATA_PART_LABEL}`]);
-		await childProcess('mount', ['-v', `${dev}`, `${DATA_MOUNT_PATH}`]);
+		// mount the data partition
+		const dev = await await childProcess('blkid', ['-L', DATA_PART_LABEL]);
+		await childProcess('mount', ['-v', dev, DATA_MOUNT_PATH]);
+
+		// set container hostname to the device uuid
+		await childProcess('hostname', [BALENA_DEVICE_UUID]);
+
+		// execute a dry run
 		await dryRun();
 	} catch (e) {
 		logger.error(e);
